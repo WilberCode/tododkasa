eea7e9539e288f02283311ddebb3a4673a0475ff
wilber <parionahuaraca@gmail.com>
2020-07-27 20:01:07 -0500

    cambio2

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <!-- Site footer markup goes here -->
-<footer class=" footer bg-dark w-full py-6 ">
+<footer class=" footer bg-dark w-full py-6 "> 
     <div class="container text-white">
         <?php  dynamic_sidebar('footer-info') ?>
     </div>
