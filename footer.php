dc480d754a36f09a72cf0a94c8abd593f1b7a003
wilber <parionahuaraca@gmail.com>
2020-07-27 19:57:15 -0500

    cambios

--- footer.php
+++ footer.php
@@ -1,11 +1,9 @@
 <!-- Site footer markup goes here -->
-<footer class=" footer bg-dark w-full py-6      ">
+<footer class=" footer bg-dark w-full py-6 ">
     <div class="container text-white">
         <?php  dynamic_sidebar('footer-info') ?>
     </div>
 </footer>
 <?php wp_footer(); ?>   
-</body>
-
-
+</body> 
 </html>
