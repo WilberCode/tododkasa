b9a160ff52223bdd0c8b7931b7c48e957711af53
wilber <parionahuaraca@gmail.com>
2020-07-30 18:31:04 -0500

    account styles

--- webpack.mix.js
+++ webpack.mix.js
@@ -44,7 +44,7 @@ mix.purgeCss({
     ]),
     whitelist: ['current-menu-item' ],
     // whitelistPatterns: [/^search-/],
-    whitelistPatternsChildren: [/^search-/,/^rslides_nav/, /^wp-block/,/^marca-social/,/^qubely-/,/^woocommerce/,/^products/, /^woocommerce-/,/^table/,/^single-product/]
+    whitelistPatternsChildren: [/^search-/,/^rslides_nav/, /^wp-block/,/^marca-social/,/^qubely-/,/^woocommerce/,/^products/, /^woocommerce-/,/^table/,/^single-product/,/^woocommerce-account/]
 });
 
 // if (mix.inProduction()) {
