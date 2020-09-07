eda99c227ac6aa374a9535e23ef750b4c16e977e
wilber <parionahuaraca@gmail.com>
2020-09-07 02:15:09 -0500

    cambios

---
+++ page_marca.php
@@ -0,0 +1,33 @@
+
+
+<?php
+ 
+/*
+Template Name: Marca
+Template Post Type: post, page, event
+*/ 
+
+
+get_header(); ?>
+  
+
+<div class="container ">
+<?php
+  if ( have_posts() ) :
+
+      // Start the Loop.
+      while ( have_posts() ) :
+          the_post(); 
+
+      endwhile;   
+  else : 
+
+  endif;
+  ?>  
+</div>
+    
+ 
+ 
+<?php
+
+get_footer();
