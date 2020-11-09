4af1113ce9124045aa96b0321e0f0b964cab700a
wilber <parionahuaraca@gmail.com>
2020-11-09 16:29:03 -0500

    cambiosa

--- index.php
+++ index.php
@@ -4,12 +4,10 @@ get_header(); ?>
 
 <div class="container ">
 <?php
-  if ( have_posts() ) :
-
+  if ( have_posts() ) : 
       // Start the Loop.
       while ( have_posts() ) :
-          the_post(); 
-
+          the_post();  
       endwhile;   
   else : 
 
