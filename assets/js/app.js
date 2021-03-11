6642e179545b83437023259379a4f288ec758ef5
wilber <parionahuaraca@gmail.com>
2021-03-10 21:21:45 -0500

    cambios

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,7 @@ import menuMobile from  './components/menuMobile'
 jQuery(function ($) {  
     $(document).ready(function () {  
         menuMobile($) // Menu Mobile: Show menu in mobile     
+        console.log('Test');
     });  
 });
  
